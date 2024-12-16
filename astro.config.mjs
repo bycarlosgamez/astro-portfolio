7ed768f6890d38f32ed0ab9670200bed97881746
carlosgamez-nga <carlos@ngahealthcare.com>
2024-12-16 08:30:33 -0600

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https:bycarlosgamez.com',
+  site: 'https://bycarlosgamez.com',
 });
